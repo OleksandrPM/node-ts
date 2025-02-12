d304b9c3d0653113636ddcc17a41d487fa770c6f
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-12 16:55:27 +0100

    add helpers ApiREsponse & ApiError

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,5 +1,7 @@
 import { Body, Get, JsonController, Param, Post } from "routing-controllers";
 import { IPerson } from "./Person.types";
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
 
 // In real project we save data in a DB
 const storeData: IPerson[] = [];
@@ -8,22 +10,29 @@ const storeData: IPerson[] = [];
 export default class Person {
   @Get()
   async getAll() {
-    return storeData;
+    return new ApiResponse(true, storeData);
   }
 
   @Get("/:id")
-  async getOne(@Param("id") id: number): Promise<IPerson | {}> {
+  async getOne(@Param("id") id: number): Promise<ApiResponse<IPerson | {}>> {
     const person = storeData.find((item) => {
       return item.id === id;
     });
 
-    return person || {};
+    if (!person) {
+      throw new ApiError(404, {
+        code: "PERSON_NOT_FOUND",
+        message: `Person with id ${id} not found`,
+      });
+    }
+
+    return new ApiResponse(true, person);
   }
 
   @Post()
   async setPerson(@Body() body: IPerson) {
     storeData.push(body);
 
-    return true;
+    return new ApiResponse(true, "Person successfully created");
   }
 }
